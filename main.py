47b31c2ff715161304fba1acaff98b62986315b8
Tindahood <timhrtn@outlook.de>
2023-10-06 05:02:55 +0200

    [propose] simple interest rate generation

--- main.py
+++ main.py
@@ -1,52 +1,8 @@
-#import certifi as certifi
-#import motor as motor
-#import pymongo as pymongo
 from fastapi import FastAPI
-#import motor.motor_asyncio
-
-
-
+from random import uniform
 
 app = FastAPI()
-"""
-username = "mongo"
-password = "mongo"
-mongo_db_connection_string = "mongodb+srv://" \
-                             "{username}:{password}@cluster0.gvyd35q.mongodb.net/?retryWrites=true" \
-                             "&w=majority".format(username=username, password=password)
-#client = pymongo.MongoClient(mongo_db_connection_string)
 
-#client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
-
-#trying somehting new
-#tlsCAFile=certifi.where()
-#client = motor.motor_asyncio.AsyncIOMotorClient(mongo_db_connection_string)
-client = motor.motor_asyncio.AsyncIOMotorClient(mongo_db_connection_string, tlsCAFile=certifi.where())
-db = client.sample_training
-
-@app.get("/")
-async def root():
-
-    #db = client.sample_training
-    #collection = await db.companies
-    #entry = await collection.find_one()
-    #entry = await db["students"].find_one().to_list(1000)
-    #entry = await db["companies"].find_one()
-    entry = await db["companies"].find().to_list(1000)
-    print(entry)
-
-    return {"message": "Hello World"}
-
-"""
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-@app.get("request_credit")
-async def reqest_credit():
-
-    return 200
-
-@app.post("bid_on_auction")
-async def bid_on_auction(id: str):
-    return 200
+@app.post("/{n}")
+async def place_bid(amount: int, use_case: str, loan_time: int):
+    return { 'interest_rate' : uniform(2.5, 10.) }
